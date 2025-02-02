09354c6eb819771a319d10b17a62aaa5de11ff87
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2025-02-02 12:46:47 +0500

    Some ready

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+
+  win.loadURL('https://my.hemis.uz/');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
