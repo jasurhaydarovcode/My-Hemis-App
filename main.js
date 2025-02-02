445ca84c0ec4313d28ae2c1721b6539f878177c6
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2025-02-02 13:07:43 +0500

    Ready for run

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 800,
+    icon: path.join(__dirname, 'icon.ico'),
     webPreferences: {
       nodeIntegration: false
     }
